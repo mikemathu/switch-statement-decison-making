42d615d95f43f08f702bff8266015f298d1383fa
Michael Mathu <mathumichael17@gmail.com>
2022-05-09 13:15:43 +0300

    pushing commit

--- Program.cs
+++ Program.cs
@@ -4,27 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your score");
-            string score = Console.ReadLine();
-            int grade = Convert.ToInt32(score);
+            //SWITCH GRADING SYSTEM
+            //Console.WriteLine("Enter your score");
+            //string score = Console.ReadLine();
+            //int grade = Convert.ToInt32(score);
 
-            switch (grade)
-            {
-                case >= 80:
-                    Console.WriteLine("Your grade is A");
-                    break;
-                case >= 60:
-                    Console.WriteLine("Your grade is B");
-                    break ;
-                case >= 50:
-                    Console.WriteLine("Your grade is C");
-                    break;
-                default:
-                    Console.WriteLine("You failed!");
-                    break;
-            }
+            //switch (grade)
+            //{
+            //   case >= 80:
+            //Console.WriteLine("Your grade is A");
+            //break;
+            //case >= 60:
+            //Console.WriteLine("Your grade is B");
+            //break ;
+            //case >= 50:
+            //Console.WriteLine("Your grade is C");
+            //break;
+            //default:
+            //Console.WriteLine("You failed!");
+            //break;
+            //}
+
+            //CALCULATOR
+            Console.WriteLine("Enter the first number");
+            string firstInput = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(firstInput);
+
+            Console.WriteLine("Enter the second number");
+            string secondInput = Console.ReadLine();
+            int secondNumber = Convert.ToInt32(secondInput);
 
 
+            Console.WriteLine("Enter the computation sign");
+            string computationSign = Console.ReadLine();
+
+            int answer = firstNumber + secondNumber;
+
+            //Console.WriteLine("Your answer is "+ answer);
+            Console.WriteLine("Your answer is {0}", answer);
 
         }
     }
